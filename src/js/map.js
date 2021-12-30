af992993c5b7f2d07a6c1e289fbba06dbd08b9dc
Tina Huynh <tinamchuynh@gmail.com>
2021-12-29 21:04:22 -0800

    change source of map api

--- src/js/map.js
+++ src/js/map.js
@@ -1,11 +1,5 @@
-var map = L.map('map').setView([33.791489, -117.853104], 12);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoidC1tYy1odXluaCIsImEiOiJja3hyNzk1N2Y0aXJ1Mndva3F2eGFxYXA1In0.ftFKSKwU0CQrU0rwFvjETQ'
-}).addTo(map);
-
 var marker = L.marker([33.791489, -117.853104]).addTo(map);
+
+function GetMap() {
+    var map = new Microsoft.Maps.Map('#map');
+}
