43e95d409215990624e1f0d5f797c5b99cdb8249
Tina Huynh <tinamchuynh@gmail.com>
2022-01-01 14:44:03 -0800

    display details from api

--- src/js/details.js
+++ src/js/details.js
@@ -8,6 +8,15 @@ $.ajax({
         "$$app_token": TOKEN.MUNI_TOKEN
     }
 }).done(function(data) {
+    var container = document.getElementById("details");
+    for (var i = 0; i < 5; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = data[i].parceladdress + ", " + data[i].state + " " + data[i].zip;
+        container.appendChild(div);
+        var div2 = document.createElement("div");
+        div2.innerHTML = 'Bedrooms: ' + data[i].bedrooms + ' Full Bathrooms: ' + data[i].numfullbaths + ' Half Bathrooms: ' + data[i].numhalfbaths;
+        container.appendChild(div2);
+    }
     console.log(data);
 });
 
