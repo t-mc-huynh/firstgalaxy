b4cb6a0f52116c505e4a0746bb32cedce5b47659
Tina Huynh <tinamchuynh@gmail.com>
2022-01-16 22:57:03 -0800

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,8 @@
-import { school } from "./data.js";
+import { school, cities } from "./data.js";
 var found = false;
 //console.log(school);
 
+//console.log(cities);
 // Prevents form submission for debugging purposes
 var form = document.getElementById("search-form");
 var submitBtn = document.querySelector(".main-button");
@@ -11,7 +12,7 @@ function handleForm(evt) {
 }
 form.addEventListener("submit", handleForm);
 
-form.addEventListener('keydown', runSearches, true);
+form.addEventListener("keydown", runSearches, true);
 submitBtn.addEventListener("click", runSearches, true);
 
 function runSearches(e) {
@@ -23,7 +24,7 @@ function runSearches(e) {
             // User pressed enter 
             var input;
             input = document.querySelector(".searchText").value;
-            console.log(input);
+            console.log("Input: " + input);
             input = input.toLowerCase();
             input = input.trim();
 
@@ -34,7 +35,7 @@ function runSearches(e) {
             }
 
             searchCity(input);
-            searchDistrict(input);
+            searchSchoolData(input);
 
             // Done running all the searches 
             if (found == false) {
@@ -43,31 +44,30 @@ function runSearches(e) {
         }
 
     }
-
-
 }
 
 function searchCity(input) {
-    const LAOCcities = ["Acton", "Adams - Normandie", "Agoura Hills", "Agua Dulce", "Alhambra", "Aliso Viejo", "Alondra Park", "Altadena", "Anaheim", "Antelope Acres", "Arcadia", "Arleta", "Arlington Heights", "Artesia", "Athens", "Atwater Village", "Avalon", "Avocado Heights", "Azusa", "Baldwin Hills", "Baldwin Park", "Bandini", "Bassett", "BelAir", "Bell", "Bell Gardens", "Bellflower", "Beverly Crest", "Beverly Glen", "Beverly Grove", "Beverly Hills", "Beverlywood", "Bouquet Canyon", "Boyle Heights", "Bradbury", "Brea", "Brentwood", "Broadway-Manchester", "BuenaPark", "Burbank", "Calabasas", "CanogaPark", "CanyonCountry", "Carson", "Carthay", "Castaic", "Central Avenue", "Cerritos", "Charter Oak", "Chatsworth", "Chinatown", "Citrus", "City of Industry", "Claremont", "Commerce", "Compton", "CostaMesa", "CotodeCaza", "Covina", "Crenshaw", "Crystalaire", "Cudahy", "CulverCity", "Cypress", "DanaPoint", "DelAire", "DelSur", "DiamondBar", "Downey", "Duarte", "EagleRock", "East Los Angeles", "East Pasadena", "East Rancho Dominguez", "East San Gabriel", "Echo Park", "El Camino Village", "El Monte", "El Segundo", "El Sereno", "Elizabeth Lake", "Encino", "Expo Park", "Fairmont", "Firestone Park", "Florence", "Fountain Valley", "Fullerton", "Garden Grove", "Gardena", "Glassell Park", "Glendale", "Glendora", "Gorman", "Graham", "Granada Hills", "Green Meadows", "Green Valley", "Hacienda Heights", "Hancock Park", "Harbor City", "Hawaiian Gardens", "Hawthorne", "Hermosa Beach", "Hi Vista", "Hidden Hills", "Highland Park", "Hollywood", "Huntington Beach", "Huntington Park", "Inglewood", "Irvine", "Irwindale", "Juniper Hills", "Kagel Canyon", "Kinneloa Mesa", "Koreatown", "La Canada Flintridge", "La Crescenta", "La Habra", "La Habra Heights", "La Mirada", "La Palma", "La Puente", "La Verne", "Ladera Heights", "Ladera Ranch", "Laguna Beach", "Laguna Hills", "Laguna Niguel", "Laguna Woods", "Lake Balboa", "Lake Forest", "Lake Hughes", "Lake Los Angeles", "Lake View Terrace", "Lakewood", "Lancaster", "Las Flores", "Lawndale", "Lennox", "Leona Valley", "Lincoln Heights", "Littlerock", "Llano", "Lomita", "Long Beach", "Los Alamitos", "Los Angeles", "Los Feliz", "Los Nietos", "Lynwood", "Malibu", "Manhattan Beach", "Mar Vista", "Marina Del Rey", "Maywood", "Midway City", "Mid-Wilshire", "Mint Canyon", "Miracle Mile", "Mission Hills", "Mission Viejo", "Monrovia", "Monte Nido", "Montebello", "Montecito Heights", "Monterey Park", "Montrose", "Mount Washington", "Newhall", "NewportBeach", "NorthElMonte", "NorthHills", "NorthHollywood", "NorthTustin", "NorthWhittier", "NortheastAntelopeValley", "Northridge", "NorthwestPalmdale", "Norwalk", "Orange", "Pacific Palisades", "Pacoima", "Palmdale", "Palms", "Palos Verdes Estates", "Palos Verdes Peninsula", "Panorama City", "Paramount", "Pasadena", "Pearblossom", "PicoRivera", "Pico-Robertson", "Pico-Union", "Placentia", "Playa Del Rey", "Playa Vista", "Pomona", "Porter Ranch", "Quartz Hill", "Ramona", "Rancho Dominguez", "Rancho Palos Verdes", "Rancho Park", "Rancho Santa Margarita", "Redondo Beach", "Reseda", "Rolling Hills", "Rolling Hills Estates", "Rosemead", "Rowland Heights", "San Clemente", "San Dimas", "San Fernando", "San Gabriel", "San Juan Capistrano", "San Marino", "San Pasqual", "San Pedro", "Santa Ana", "Santa Clarita", "Santa Fe Springs", "Santa Monica", "Sawtelle", "Seal Beach", "Shadow Hills", "Sherman Oaks", "Sierra Madre", "Signal Hill", "Silver Lake", "South El Monte", "South Gate", "South Park", "South Pasadena", "South San Gabriel", "South San Jose Hills", "South Whittier", "Southeast Antelope Valley", "Stanton", "Stevenson Ranch", "Studio City", "Sun Valley", "Sun Village", "Sunland", "Sylmar", "Tarzana", "Temple City", "Toluca Lake", "Topanga", "Torrance", "Tujunga", "Tujunga Canyons", "Tustin", "Unincorporated Catalina Island", "Unincorporated Santa Monica Mountains", "Universal City", "University Park", "Val Verde", "Valencia", "Valinda", "Valley Glen", "Valley Village", "Valyermo", "Van Nuys", "Venice", "Verdugo City", "Vermont Knolls", "Vermont Square", "Vermont Vista", "Vermont-Slauson", "Vernon", "View Park-Windsor Hills", "Villa Park", "Vincent", "Walnut", "Walnut Park", "Watts", "West Athens", "West Carson", "West Compton", "West Covina", "West Hills", "West Hollywood", "West Los Angeles", "West Puente Valley", "West Whittier", "Westchester", "Westlake", "Westlake Village", "Westminster", "Westmont", "Westwood", "Whittier", "Willowbrook", "Wilmington", "Winnetka", "Woodland Hills", "Yorba Linda"];
-
-    for (let i = 0; i < LAOCcities.length; i++) {
-        LAOCcities[i] = LAOCcities[i].toLowerCase();
-    }
-
-    if (LAOCcities.includes(input)) {
-        console.log("City found in LA or OC county");
-        found = true;
-    }
-}
-
-function searchDistrict(input) {
-    for (let i = 1; i < school.length; i++) {
-        if (input == school[i][5].toLowerCase()) {
+    for (let i = 0; i < cities.length; i++) {
+        if (cities[i][0].toLowerCase().includes(input)) {
+            console.log("City found in LA or OC county");
             found = true;
-            console.log("Input found is district list");
-            break;
         }
     }
 }
 
-export { runSearches };
+function searchSchoolData(input) {
+    for (let i = 1; i < school.length; i++) {
+        if (school[i][5].toLowerCase().includes(input) || input == school[i][6].toLowerCase().includes(input)) {
+            found = true;
+            //console.log("Input found is district or school list");
+            if (school[i][6].toLowerCase().includes(input)) {
+                // prints the district of the school
+                console.log(school[i][5] + " is the district of the inputted school");
+                break;
+            } else {
+                // prints all schools in that district with the type of school it is
+                console.log(school[i][6] + " - " + school[i][14]);
+            }
+        }
+    }
+}
