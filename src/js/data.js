03bddcc86d6342eaf2cce4d0dbbf87cb84494669
Tina Huynh <tinamchuynh@gmail.com>
2022-01-17 12:28:31 -0800

    sort terms and definitions into pg from excel

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,10 @@
+"use strict"
+
 const result = new Array();
 const school = new Array();
 const glossary = new Array();
 const cities = new Array();
+let glossary_container = document.getElementById("parent-container");
 //console.log(result);
 //console.log(school);
 //console.log(glossary);
@@ -41,16 +44,25 @@ $(function() {
             cities.push(result[2][1][m]);
         }
 
-        sortTerms(container);
+        sortTerms(glossary_container);
         //console.log(result);
     }
     oReq.send();
 });
 
-
-
-
-var container = document.getElementById("parent-container");
+function sortTerms(glossary_container) {
+    let term_container = glossary_container.children;
+    for (let j = 0; j < term_container.length; j++) {
+        if (term_container[j].classList.contains("glossary-key-header")) {
+            for (let k = 1; k < glossary.length; k++) {
+                if (glossary[k][0].toString().charAt(0) == term_container[j].firstElementChild.innerHTML) {
+                    term_container[j + 1].appendChild(createList(glossary[k][0]));
+                }
+            }
+        }
+    }
+    getLinks();
+}
 
 function createList(term) {
     var list = document.createElement("li");
@@ -63,32 +75,30 @@ function createList(term) {
     link.innerHTML = term;
     link.setAttributeNode(toggle);
     link.setAttributeNode(target);
-    link.addEventListener("click", function() { infoModal(link) }, false);
     list.appendChild(link);
     return list;
 }
 
-function sortTerms(container) {
-    var children = container.children;
-    for (var j = 0; j < children.length; j++) {
-        if (children[j].classList.contains("glossary-key-header")) {
-            for (var k = 1; k < glossary.length; k++) {
-                if (glossary[k][0].toString().charAt(0) == children[j].firstElementChild.innerHTML) {
-                    children[j + 1].appendChild(createList(glossary[k][0]));
-                }
-            }
-        } else if (children[j].classList.contains("glossary-term-container")) {
+function getLinks() {
+    let links = document.querySelectorAll(".glossary-term-container li a");
+    links.forEach(function(clicked) {
+        clicked.addEventListener("click", function() { infoModal(clicked) }, false);
+    });
+}
 
+function infoModal(link) {
+    let modal_label = document.getElementById("DEFINITION_MODAL_LABEL");
+    let modal_body = document.getElementById("DEFINITION_MODAL_BODY");
+
+    let term = link.innerHTML.trim();
+    for (let i = 1; i < glossary.length; i++) {
+        if (glossary[i][0].trim() == term) {
+            modal_label.innerHTML = glossary[i][0];
+            modal_body.innerHTML = glossary[i][1];
         }
     }
+    modal_label.style.fontWeight = "bold";
 }
-
-function infoModal(e) {
-    console.log(e);
-}
-
-
-
 
 
 export { result, school, glossary, cities };
