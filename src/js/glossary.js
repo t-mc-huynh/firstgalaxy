880b6c33731523672c6dadc66b5cbbf3fef481de
Tina Huynh <tinamchuynh@gmail.com>
2022-01-16 22:57:22 -0800

    Update glossary.js

--- src/js/glossary.js
+++ src/js/glossary.js
@@ -1,18 +1,26 @@
-function switchTxT(x) {
+import { glossary } from "./data.js";
+
+var section = document.querySelectorAll(".glossary-term-container li a");
+section.forEach(function(e) {
+    //e.addEventListener("click", switchTxT);
+    e.addEventListener("click", function() { switchTxT(e) }, false);
+});
+
+function switchTxT(e) {
     let term, def, a, t_el, t_id, t, d;
     term = document.getElementById("DEFINITION_MODAL_LABEL");
     def = document.getElementById("DEFINITION_MODAL_BODY");
 
-    t_id = x.id;
-    t_el = document.getElementById(t_id);
-    d = t_el.nextElementSibling.innerHTML;
-    t = t_el.innerHTML;
-
-    console.log(t_id);
+    console.log(e);
+    t = e.innerHTML.trim();
+    //t = document.querySelector(#x.delegateTarget);
     console.log(t);
-    console.log(d);
-
+    for (let i = 1; i < glossary.length; i++) {
+        if (glossary[i][0].trim() == t) {
+            term.innerHTML = glossary[i][0];
+            def.innerHTML = glossary[i][1];
+            break;
+        }
+    }
     term.style.fontWeight = "bold";
-    term.innerHTML = t;
-    def.innerHTML = d;
 }
