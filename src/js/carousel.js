1715cfd52a08a08a7992d8872270ae299f2d1284
t-mc-huynh <tinamchuynh@gmail.com>
2022-01-19 22:40:54 -0800

    get nearby cities

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -19,6 +19,9 @@ $(document).ready(function() {
 
 var IP = "23.240.76.211";
 
+const nearbyCities = new Array();
+nearbyCities.push(["City Name", "State", "Latitude", "Longitude", "Location Code"]);
+
 $.getJSON("https://api.ipify.org/?format=json", function(data) {
 
     // Setting text of element P with id gfg
@@ -31,4 +34,20 @@ $.getJSON('https://api.techniknews.net/ipgeo/' + IP, function(input) {
     // Setting text of element P with id gfg
     console.log(input);
     console.log(Object.entries(input).length);
+});
+
+$.getJSON("http://getnearbycities.geobytes.com/GetNearbyCities?callback=?&radius=100&locationcode=" + IP, function(data) {
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            let temp = new Array();
+            temp.push(data[i][1]);
+            temp.push(data[i][2]);
+            temp.push(data[i][8]);
+            temp.push(data[i][10]);
+            temp.push(data[i][9]);
+            nearbyCities.push(temp);
+            break;
+        }
+    }
+    console.log(nearbyCities);
 });
