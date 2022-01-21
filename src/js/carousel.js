7719f7a324828a0aaf9fb80a8ff326e57ed8a4dd
t-mc-huynh <tinamchuynh@gmail.com>
2022-01-21 13:14:44 -0800

    load carousel with photos

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,23 +2,6 @@ import { TOKEN } from './app-env.js';
 
 import { result } from "./data.js";
 
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        center: true,
-        items: 2,
-        loop: true,
-        margin: 10,
-        autoplay: false,
-        autoplayTimeout: 1000,
-        autoplayHoverPause: true,
-        responsive: {
-            600: {
-                items: 4
-            }
-        }
-    });
-});
-
 var IP = "23.240.76.211";
 
 const nearbyCities = new Array();
@@ -31,13 +14,6 @@ $.getJSON("https://api.ipify.org/?format=json", function(data) {
     // Setting text of element P with id gfg
     // console.log(data.ip);
     IP = data.ip;
-});
-
-$.getJSON('https://api.techniknews.net/ipgeo/' + IP, function(input) {
-
-    // Setting text of element P with id gfg
-    // console.log(input);
-    // console.log(Object.entries(input).length);
 });
 
 $.getJSON("http://getnearbycities.geobytes.com/GetNearbyCities?callback=?&radius=100&locationcode=" + IP, function(data) {
@@ -61,24 +37,66 @@ setTimeout(result.push(["City Info", nearbyCities]), 5000);
 
 
 function getPictures() {
-    for (let i = 0; i < nearbyCities.length; i++) {
+    for (let i = 1; i < nearbyCities.length; i++) {
+        let index = rndIndex();
+        console.log(index);
         if (!(nearbyCities[i][5].length > 3)) {
-            fetch("https://api.unsplash.com/photos/?client_id=" + TOKEN.UNSPLASH + "&page=1&per_page=2&query=" + nearbyCities[i][1], {
+            fetch("https://api.unsplash.com/collections/461370/photos/?client_id=" + TOKEN.UNSPLASH + "&page=1&per_page=10", {
                     headers: {
                         Authorization: TOKEN.UNSPLASH
                     }
                 })
                 .then(resp => {
-                    return resp.json()
+                    return resp.json();
                 })
                 .then(data => {
-                    if (data[0].height > data[1].height) {
-                        nearbyCities[i][5] = data[0].urls.full;
-                    } else {
-                        nearbyCities[i][5] = data[1].urls.full;
-                    }
+                    console.log(data);
+                    nearbyCities[i][5] = data[i].urls.full;
+                    createCarousel(nearbyCities[i])
                 })
         }
     }
+    loadCarousel();
     console.log(nearbyCities);
+}
+
+function rndIndex() {
+    return Math.round(Math.random() * 10);
+}
+
+function createCarousel(data) {
+    var section = document.querySelector(".owl-stage");
+
+
+    var slide = document.createElement("div");
+    slide.classList.add("owl-item");
+    var photo = document.createElement("img");
+    var source = document.createAttribute("src");
+    source.value = data[5];
+    photo.setAttributeNode(source);
+    var button = document.createElement("button");
+    button.innerHTML = data[0];
+    var type = document.createAttribute("type");
+    type.value = "button";
+    button.setAttributeNode(type);
+    slide.appendChild(photo);
+    slide.appendChild(button);
+    section.appendChild(slide);
+}
+
+function loadCarousel() {
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        items: 2,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            600: {
+                items: 4
+            }
+        }
+    });
 }
