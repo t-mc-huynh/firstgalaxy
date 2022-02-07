0179710199476e7f7f822b8ff924a4b1eb5391dc
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-07 15:19:05 -0800

    add house section

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 $.getJSON("https://api.ipify.org?format=json", function(data) {
     $.getJSON("https://ipapi.co/" + data.ip + "/json/", function(response) {
         console.log(response.city, response.region_code);
