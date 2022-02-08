36409debd003f43f9629b9ce2a43d88d2084c789
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-07 16:44:30 -0800

    relocate house grid to buy pg

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,1 @@
 "use strict";
-
-
-
-$.getJSON("https://api.ipify.org?format=json", function(data) {
-    $.getJSON("https://ipapi.co/" + data.ip + "/json/", function(response) {
-        console.log(response.city, response.region_code);
-    })
-})
