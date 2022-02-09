b89f24398acc312d964e9ed263ee061310ea0d6a
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-09 13:05:45 -0800

    separate join from nav

---
+++ src/js/join.js
@@ -0,0 +1,19 @@
+// FAQ accordian
+$(function() {
+    $('.accordian li').on('click', function() {
+        if ($(this).find('ul').hasClass('open')) {
+            $('ul.open').slideToggle().removeClass('open');
+            $('.accordian h3 span').removeClass('closed');
+            $('.accordian h3').removeClass('closed');
+        } else {
+            $('ul.open').slideToggle().removeClass('open');
+            $(this).find('ul').slideToggle().addClass('open');
+            $('.accordian h3, .accordian h3 span').removeClass('closed');
+            $(this).find('h3, h3 span').addClass('closed')
+        }
+    });
+    // Active class starts one open
+    $('.accordian li.active ul').slideDown().addClass('open');
+});
+
+// End
