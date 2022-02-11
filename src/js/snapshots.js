782bbc1332a76bb7b6d234dd5c458ff37ed606a3
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-11 15:41:28 -0800

    infographics dynamically uploaded

--- src/js/snapshots.js
+++ src/js/snapshots.js
@@ -1,3 +1,43 @@
 "use strict";
+var image_container = document.getElementsByClassName("nar-images")[0];
+console.log(image_container);
 
-var image_container = document.getElementsByClassName("nar-images");
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFuntion(this);
+    }
+};
+xhttp.open("GET", "src/data/snapshots/", true);
+xhttp.send();
+
+const filePaths = new Array();
+
+/**
+ * Gets the file path for all files in the snapshots folder
+ * @param {*} xml 
+ */
+function myFuntion(xml) {
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(xml.responseText, 'text/html');
+    var preList = htmlDoc.getElementsByTagName("li");
+    for (let i = 1; i < preList.length; i++) {
+        console.log(preList[i].firstChild.getAttribute("href"));
+        filePaths.push(preList[i].firstChild.getAttribute("href"));
+    }
+    uploadImage();
+}
+
+console.log(filePaths);
+
+function uploadImage() {
+    for (let i = 0; i < filePaths.length; i++) {
+        var filePath = filePaths[i].toString();
+        var image = document.createElement("img");
+        var source = document.createAttribute("src");
+        source.value = filePath;
+        image.setAttributeNode(source);
+        image_container.appendChild(image);
+        console.log("yes");
+    }
+}
