6d2d2b12342b674b690332116795685f8537aa6f
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-19 16:56:24 -0800

    Update buy.js

--- src/js/buy.js
+++ src/js/buy.js
@@ -1,7 +1,7 @@
+import { location, geoloc } from "./carousel.js";
+
 "use strict";
 
-$.getJSON("https://api.ipify.org?format=json", function(data) {
-    $.getJSON("https://ipapi.co/" + data.ip + "/json/", function(response) {
-        console.log(response.city, response.region_code);
-    })
-})
+if (geoloc) {
+
+}
