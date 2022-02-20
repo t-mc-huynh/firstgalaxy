0e2ff34a16f821f1577ec4ccddc86b5da309dbea
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-19 21:44:44 -0800

    add missing items from search bar

--- src/js/buy.js
+++ src/js/buy.js
@@ -1,7 +1,10 @@
-import { location, geoloc } from "./carousel.js";
+import { geo_location, geoloc } from "./carousel.js";
+import { ip_location } from "./search.js";
 
 "use strict";
 
 if (geoloc) {
-
+    console.log(geo_location);
+} else {
+    console.log(ip_location);
 }
