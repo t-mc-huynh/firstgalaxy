711d22af52d127bf783e4ed1589d4553d8870c04
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-23 22:27:50 -0800

    add calculation functionality | graph updating needs to be added

--- src/js/commission.js
+++ src/js/commission.js
@@ -1,72 +1,121 @@
 "use strict";
 
 var salePrice = document.getElementById("commissionRange");
-var salePriceLabel = salePrice.previousElementSibling;
+var salePriceLabel = salePrice.previousElementSibling.previousElementSibling;
 var text = salePriceLabel.innerHTML.split(" | ");
 var dollarAmount = text[0];
-console.log(text);
-console.log(salePriceLabel);
+var agentSelect = document.getElementById("agentCommission");
+var brokerageSelect = document.getElementById("brokeragePercent");
+var commission_from_sale, agent_takes_home;
+var takes_home_with_us = document.getElementById("withUs");
+var takes_home_elsewhere = document.getElementById("withThem");
+var commissionSplit = new Array();
+commissionSplit = [90, 10];
+
 salePrice.oninput = function() {
-    dollarAmount = "$" + numberWithCommas(this.value);
+    dollarAmount = "$" + numberWithCommas(salePrice.value);
     salePriceLabel.innerHTML = dollarAmount + " | " + text[1];
+
+    agentCommission = agentSelect.options[agentSelect.selectedIndex].text;
+    brokeragePercent = brokerageSelect.options[brokerageSelect.selectedIndex].text;
+
+    commission_from_sale = salePrice.value * percentToDecimail(agentCommission);
+
+    agent_takes_home = commission_from_sale - (commission_from_sale * percentToDecimail(brokeragePercent));
+
+    takes_home_elsewhere.innerHTML = "Your commission | " + "$" + numberWithCommas(agent_takes_home);
+
+    commissionSplit = new Array();
+    commissionSplit.push(parseFloat(brokeragePercent));
+    commissionSplit.push(100 - parseFloat(brokeragePercent));
+}
+
+var agentCommission, brokeragePercent;
+
+agentSelect.addEventListener("change", updateNumbers, true);
+brokerageSelect.addEventListener("change", updateNumbers, true);
+
+function updateNumbers(e, price = salePrice.value, agentCommission, brokeragePercent) {
+    dollarAmount = "$" + numberWithCommas(salePrice.value);
+    salePriceLabel.innerHTML = dollarAmount + " | " + text[1];
+
+
+    agentCommission = agentSelect.options[agentSelect.selectedIndex].text;
+    brokeragePercent = brokerageSelect.options[brokerageSelect.selectedIndex].text;
+
+    commission_from_sale = price * percentToDecimail(agentCommission);
+
+    agent_takes_home = commission_from_sale - (commission_from_sale * percentToDecimail(brokeragePercent));
+
+    takes_home_elsewhere.innerHTML = "Your commission | " + "$" + numberWithCommas(agent_takes_home);
 }
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-var agentSelect = document.getElementById("agentCommission");
-var agentCommission;
-agentSelect.addEventListener("change", function() {
-    console.log("agentCommission", agentSelect.options[agentSelect.selectedIndex].text);
-    agentCommission = agentSelect.options[agentSelect.selectedIndex].text;
-}, true);
+function percentToDecimail(x) {
+    let dec = parseFloat(x);
+    console.log(dec);
+    let percentage = dec / 100;
+    console.log(percentage);
+    return percentage;
+}
 
-var brokerageSelect = document.getElementById("brokeragePercent");
-var brokeragePercent;
-brokerageSelect.addEventListener("change", function(e) {
-    console.log("brokeragePercent", brokerageSelect.options[brokerageSelect.selectedIndex].text);
-    brokeragePercent = brokerageSelect.options[brokerageSelect.selectedIndex].text;
-}, true);
 
-const DATA_COUNT = 2;
-const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
 
-const data = {
-    labels: ['Red', 'Orange'],
-    datasets: [{
-        label: 'Dataset 1',
-        data: [80, 20],
-        backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)'
-        ],
-    }]
-};
-
-const config = {
-    type: 'doughnut',
-    data: data,
+new Chart(document.getElementById("FG_chart"), {
+    type: "doughnut",
+    data: {
+        datasets: [{
+            backgroundColor: ["#aa0000", "#303031"],
+            data: [80, 20]
+        }],
+        labels: ["Your Commission", "Brokerage Commission"]
+    },
     options: {
-        responsive: true,
+        title: {
+            display: false,
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInOutCubic'
+        },
         plugins: {
             legend: {
                 display: false,
             },
-            title: {
-                display: true,
-                text: 'Chart.js Doughnut Chart'
-            }
+            tooltip: {
+                enabled: false,
+            },
         }
+    }
+});
+
+var second = new Chart(document.getElementById("them_chart"), {
+    type: "doughnut",
+    data: {
+        datasets: [{
+            backgroundColor: ["#aa0000", "#303031"],
+            data: commissionSplit
+        }],
+        labels: ["Your Commission", "Brokerage Commission"]
     },
-};
-
-const chart1 = new Chart(
-    document.getElementById('chart1'),
-    config
-);
-
-const chart2 = new Chart(
-    document.getElementById('chart2'),
-    config
-);
+    options: {
+        title: {
+            display: false,
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInOutCubic'
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                enabled: false,
+            },
+        }
+    }
+});
