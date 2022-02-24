ae96d0eb6efd40c07e558aecb99df5b07f36406c
t-mc-huynh <tinamchuynh@gmail.com>
2022-02-23 19:05:10 -0800

    add doughnut charts

--- src/js/commission.js
+++ src/js/commission.js
@@ -7,14 +7,10 @@ var dollarAmount = text[0];
 console.log(text);
 console.log(salePriceLabel);
 salePrice.oninput = function() {
-        dollarAmount = "$" + numberWithCommas(this.value);
-        console.log(dollarAmount);
-        salePriceLabel.innerHTML = dollarAmount + " | " + text[1];
-    }
-    // salePrice.addEventListener("onchange", function() {
-    //     salePriceLabel = "$" + this.value;
-    //     console.log(salePriceLabel);
-    // }, true);
+    dollarAmount = "$" + numberWithCommas(this.value);
+    salePriceLabel.innerHTML = dollarAmount + " | " + text[1];
+}
+
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -32,3 +28,45 @@ brokerageSelect.addEventListener("change", function(e) {
     console.log("brokeragePercent", brokerageSelect.options[brokerageSelect.selectedIndex].text);
     brokeragePercent = brokerageSelect.options[brokerageSelect.selectedIndex].text;
 }, true);
+
+const DATA_COUNT = 2;
+const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
+
+const data = {
+    labels: ['Red', 'Orange'],
+    datasets: [{
+        label: 'Dataset 1',
+        data: [80, 20],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)'
+        ],
+    }]
+};
+
+const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Doughnut Chart'
+            }
+        }
+    },
+};
+
+const chart1 = new Chart(
+    document.getElementById('chart1'),
+    config
+);
+
+const chart2 = new Chart(
+    document.getElementById('chart2'),
+    config
+);
